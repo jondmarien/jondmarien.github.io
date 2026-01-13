fceadfce5e9288b942d4534f0230720858c62083
Jonathan Marien <jon@chron0.tech>
2026-01-12 19:46:58 -0500

    fix: fix some small css/layout changes; added socials + portfolio

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Chrono's Cyber Chronicles",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jondmarien/jondmarien.github.io",
-      "Discord Community": "https://discord.gg/ACP3pgmAWF",
+      LinkedIn: "https://linkedin.com/in/jondmarien",
+      Instagram: "https://instagram.com/jon.marien",
+      Portfolio: "https://chron0.tech",
     },
   }),
 }
@@ -42,8 +43,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }

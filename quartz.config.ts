732f361ef8d9285e58d9e5bcb767add497090e17
Jonathan Marien <jon@chron0.tech>
2026-01-12 19:00:33 -0500

    fix: update baseUrl and move CNAME to content directory

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.chron0.tech",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
